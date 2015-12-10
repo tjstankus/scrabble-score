1ddabcf8347ddd70b78cbc7ecc98aa6a49844772
TJ Stankus <tjstankus@gmail.com>
2015-12-09 19:42:42 -0500

    Map all letters
    All tests but last passing

--- scrabble_score.rb
+++ scrabble_score.rb
@@ -1,6 +1,12 @@
 class Scrabble
   LETTER_VALUES = {
-    1 => %w[A E I O U L N R S T]
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => %w[K],
+    8 => %w[J X],
+    10 => %w[Q Z]
   }
 
   attr_reader :word
